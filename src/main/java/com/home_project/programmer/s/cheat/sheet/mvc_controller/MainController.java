d2b5b2c0e6f6ff4ca7968c9a7f23ec1b340ad577
Alexander <89515014507@mail.ru>
2022-05-06 21:20:14 +0600

    Start ferst steps

---
+++ src/main/java/com/home_project/programmer/s/cheat/sheet/mvc_controller/MainController.java
@@ -0,0 +1,18 @@
+package com.home_project.programmer.s.cheat.sheet.mvc_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class MainController {
+    @GetMapping("/")
+    public String menu (Model model){
+        model.addAttribute("main", "Нчало работы");
+        return "main";
+    }
+    @GetMapping("/begin")
+    public String begin(Model model){
+        return "begin/begin_stady";
+    }
+}
